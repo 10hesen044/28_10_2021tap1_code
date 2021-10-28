2782e36132065c022810b1bdab35dc59411f543f
10hesen044 <tu201906250@code.edu.az>
2021-10-28 11:29:37 +0400

    project added

---
+++ 28.10.2021tap1/28.10.2021tap1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _28._10._2021tap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle, TersCumle;
+            Console.WriteLine("Cumle elave edin: ");
+            cumle = Console.ReadLine();
+            Console.WriteLine(GetWord(cumle));
+        }
+
+        static string GetWord(string cumle)
+        {
+            string TersCumle = String.Empty;
+            for (int i = cumle.Length - 1; i >= 0; i--)
+            {
+                TersCumle += cumle[i];
+            }
+            return TersCumle;
+        }
+    }
+}
